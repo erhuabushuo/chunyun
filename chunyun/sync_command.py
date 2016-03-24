ebf4568099a7812407ce44d6266e6e7cdca7576c
erhuabushuo <erhuabushuo@gmail.com>
2016-03-24 12:46:59 +0800

    sync命令检测chunyun_migrations表是否存在

--- chunyun/sync_command.py
+++ chunyun/sync_command.py
@@ -5,11 +5,27 @@ import subprocess
 
 from .command import Command
 
+HAS_MIGRATION_TABLE = '"SELECT count(*) FROM pg_tables WHERE tablename = $$chunyun_migrations$$"'
+INIT_MIGRATION_TABLE = '"CREATE TABLE IF NOT EXISTS chunyun_migrations(id serial primary key, name varchar not null, created_at TIMESTAMPTZ default CURRENT_TIMESTAMP(0));"'
+INIT_RECORD = '"INSERT INTO chunyun_migrations(name) VALUES($$001_init.sql$$)"';
 GET_LATEST_MIGRATION = '"SELECT name FROM chunyun_migrations ORDER BY ID DESC LIMIT 1"'
 NEW_MIGRATION = '"INSERT INTO chunyun_migrations(name) VALUES($${0}$$)"'
 
 
 class SyncCommand(Command):
+
+    def has_migration_table(self, option):
+        os.environ['PGPASSWORD'] = option.get(self.args.env, "password")
+        cmd = "psql -h {host} -p {port}  -U {user} -t -c {sql} {name}".format(
+                            host=option.get(self.args.env, 'host'),
+                            port=option.get(self.args.env, 'port'),
+                            user=option.get(self.args.env, 'user'),
+                            name=option.get(self.args.env, 'database'),
+                            sql=HAS_MIGRATION_TABLE)
+        handle = os.popen(cmd)
+        output = handle.read().strip()
+        handle.close()
+        return output
 
     def get_latest_migration(self, option):
         os.environ['PGPASSWORD'] = option.get(self.args.env, "password")
@@ -54,6 +70,32 @@ class SyncCommand(Command):
         cmd_handle.close()
         os.unlink(handle.name)
 
+    def create_migration_table(self, option):
+        os.environ['PGPASSWORD'] = option.get(self.args.env, "password")
+        cmd = "psql -h {host} -p {port}  -U {user} -c {sql} {name}".format(
+                            host=option.get(self.args.env, 'host'),
+                            port=option.get(self.args.env, 'port'),
+                            user=option.get(self.args.env, 'user'),
+                            name=option.get(self.args.env, 'database'),
+                            sql=INIT_MIGRATION_TABLE)
+        handle = os.popen(cmd)
+        output = handle.read()
+        handle.close()
+        print(output)
+
+    def insert_init_record(self, option):
+        os.environ['PGPASSWORD'] = option.get(self.args.env, "password")
+        cmd = "psql -h {host} -p {port}  -U {user} -c {sql} {name}".format(
+                            host=option.get(self.args.env, 'host'),
+                            port=option.get(self.args.env, 'port'),
+                            user=option.get(self.args.env, 'user'),
+                            name=option.get(self.args.env, 'database'),
+                            sql=INIT_RECORD)
+        handle = os.popen(cmd)
+        output = handle.read()
+        handle.close()
+        print(output)
+
     def get_migration_files(self):
         files = os.listdir('migrations')
         files.sort()
@@ -62,6 +104,11 @@ class SyncCommand(Command):
     def run(self):
         parser = ConfigParser()
         parser.read("config.ini")
+
+        # 检查是否有chunyun_migrations表
+        if not self.has_migration_table(parser):
+            self.create_migration_table(parser)
+            self.insert_init_record(parser)
 
         # 获取migrations表里最新同步文件
         latest_migration = self.get_latest_migration(parser)
